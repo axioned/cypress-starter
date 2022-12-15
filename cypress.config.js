174dbe0694e04d4413df9a85acc82665ce2b4fd5
Karan Patel <karanp@prdxn.com>
2022-12-15 18:25:51 +0530

    cypress config file updated

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,9 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    viewportWidth: 1440, //Global Viewport can changed here
+    viewportHeight: 900,
+    defaultCommandTimeout: 70000, //Global wait time can be changed here
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
